dde0984e89ee35ac60055e553867d939aad41c4f
Sorin Chis <kish.sorin@yahoo.com>
2020-05-09 13:42:48 +0300

    added application styles

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -1,4 +1,6 @@
 import React, { createContext, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "./styles";
 
 export const AppContext = createContext();
 
@@ -6,8 +8,16 @@ const AppProvider = ({ children }) => {
   const [themeMode, setThemeMode] = useState("lightTheme");
 
   const value = { themeMode };
+  const costumTheme = theme[themeMode];
 
-  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={value}>
+      <ThemeProvider theme={costumTheme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
 };
 
 export default AppProvider;
